eecf84e946b83a2fac059e7307f16c44238af855
AgataWtg <awasiljewa@gmail.com>
2016-02-09 18:34:32 +0300

    Изменения

--- js/check.js
+++ js/check.js
@@ -1,25 +1,25 @@
 function getMessage(a, b){
   if (typeof a === 'boolean'){
-  return (a === true) ? 'Переданное GIF-изображение анимировано и содержит ' + b + 'кадров' : 'Переданное GIF-изображение не анимировано';
+  return (a === true) ? 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров' : 'Переданное GIF-изображение не анимировано';
 }
   else if (typeof a === 'number'){
-        return 'Переданное SVG-изображение содержит' + a + 'объектов и ' + (b * 4) + 'атрибутов';
+        return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' атрибутов';
       }
 
 
   else if (Array.isArray(b) && Array.isArray(a)){
+    var square = 0;
+    var arr = [];
     for(var i = 0; i < a.length; i++) {
-      var square = 0;
-      var arr = [];
       arr.push(a[i] * b[i]);
       square += arr[i];
     }
-    return 'Общая площадь артефактов сжатия: ' + square + 'пикселей';
+    return 'Общая площадь артефактов сжатия: ' + square + ' пикселей';
   }
 
   else if (Array.isArray(a)){
-      for(var i = 0; i < a.length; i++){
       var sum = 0;
+      for(var i = 0; i < a.length; i++){
       sum += a[i];
   }
        return 'Количество красных точек во всех строчках изображения: ' + sum;
